04ac1c9e694407cdf772fd28dd5a6bd60c278712
educalle23 <edu.lds.23@gmail.com>
2025-05-28 08:08:34 -0500

    morning

--- chamber/scripts/index.js
+++ chamber/scripts/index.js
@@ -1,20 +1,29 @@
+const divCurrent = document.querySelector('·current-weather');
 const temp = document.getElementById("info-current");
 const weatherIcon = document.querySelector('#img-current');
 const captionDesc = document.createElement('p');
-
+const infoForecast = document.createElement('div');
+infoForecast.classList.add('info-forecast');
 // const myTown = document.querySelector('#myTown')
-const url = 'https://api.openweathermap.org/data/2.5/weather?lat=-8.11557&lon=-79.0&units=metric&appid=c2454891036de5dbd628eee0a593638f';
+const urlCurrent = 'https://api.openweathermap.org/data/2.5/weather?lat=-8.11557&lon=-79.0&units=metric&appid=c2454891036de5dbd628eee0a593638f';
+// const urlForecast = 'https://api.openweathermap.org/data/2.5/forecast/daily?lat=-8.11557&lon=-79.0&units=metric&appid=c2454891036de5dbd628eee0a593638f';
 
 
 async function apiFetch() {
     try {
-        const response =  await fetch(url)
-        if (response.ok){
-            const data =  await response.json();
-            console.log(data);
-            displayResults(data);
+        const [responseCurrent] = await Promise.all([
+            fetch(urlCurrent),
+        ]);
+        if (responseCurrent.ok) {
+            console.log("API call successful");
+            const dataCurrent =  await responseCurrent.json();
+            // const dataForecast = await responseForecast.json();
+            console.log(dataCurrent);
+            // console.log(dataForecast);
+            displayResultsCurrent(dataCurrent);
+            // displayResultsForecast(data);
         } else{
-            throw Error(await response.text());
+            throw Error(await responseForecast.text());
         }
     } catch (error) {
         console.log(error);
@@ -22,26 +31,8 @@ async function apiFetch() {
     
 }
 
-// function displayResults(data) {
-//     temp.innerHTML = `${data.main.temp} C°`
-//     console.log(data.main.temp_max)
-//     console.log(data.main.temp_min)
-//     console.log(data.main.humidity)
-//     console.log(new Date (data.sys.sunrise * 1000).toLocaleTimeString("en-US"));
-//     console.log(new Date (data.sys.sunset * 1000).toLocaleTimeString("en-US"));
-//     // const amanecer = new Date(data.sys.sunrise * 1000).toLocaleTimeString();
-//     // const atardecer = new Date(weatherData.sys.sunset * 1000).toLocaleTimeString();
 
-//     const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-//     weatherIcon.setAttribute('src', iconsrc);
-//     weatherIcon.setAttribute('alt', desc);
-//     // const captionDesc = data.weather[0].description;
-//     temp.innerHTML = `${data.main.temp_max}`;
-    
-
-// }
-
-function displayResults(data) {
+function displayResultsCurrent(data) {
     const tempActual = data.main.temp;
     const tempMax = data.main.temp_max;
     const tempMin = data.main.temp_min;
@@ -61,6 +52,30 @@ function displayResults(data) {
     const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
     weatherIcon.setAttribute("src", iconsrc);
     weatherIcon.setAttribute("alt", data.weather[0].description);
+    divCurrent.join(temp, weatherIcon);
 }
+
+function displayResultsForecast(data) {
+    const tempActual = data.main.temp;
+    const tempMax = data.main.temp_max;
+    const tempMin = data.main.temp_min;
+    const humedad = data.main.humidity;
+    const amanecer = new Date(data.sys.sunrise * 1000).toLocaleTimeString("en-US", { hour: "2-digit", minute: "2-digit", hour12: true });
+    const atardecer = new Date(data.sys.sunset * 1000).toLocaleTimeString("en-US", { hour: "2-digit", minute: "2-digit", hour12: true });
+
+    temp.innerHTML = `
+        <strong>Temperature:</strong> ${tempActual} C° <br>
+        <strong>Max yaraaa:</strong> ${tempMax} C° <br>
+        <strong>Min Temperature:</strong> ${tempMin} C° <br>
+        <strong>Humidity:</strong> ${humedad}% <br>
+        <strong>Sunrise:</strong> ${amanecer} <br>
+        <strong>Sunset:</strong> ${atardecer}
+    `;
+
+    const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    weatherIcon.setAttribute("src", iconsrc);
+    weatherIcon.setAttribute("alt", data.weather[0].description);
+}
+
 
 apiFetch();
