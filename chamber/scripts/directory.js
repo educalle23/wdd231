a0007444687d5c63d3d3cd173033a2ef87118b3a
educalle23 <edu.lds.23@gmail.com>
2025-06-10 12:55:23 -0500

    itmes and lazy loagind

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -27,7 +27,7 @@ document.addEventListener("DOMContentLoaded", async () => {
             const card = document.createElement("div");
             card.classList.add("item");
             card.innerHTML = `
-                <img src="images/${item.image}" alt="${item.name}">
+                <img src="images/${item.image}" alt="${item.name} loading="lazy">
 
                 <p>${item.address}</p>
                 <p>${item.phone}</p>
