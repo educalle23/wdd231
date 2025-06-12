866255c852fc5c84cbc646bc6d88047ab8f05076
educalle23 <edu.lds.23@gmail.com>
2025-06-12 18:01:37 -0500

    index and thanks done

--- finalproject/scripts/thanks.js
+++ finalproject/scripts/thanks.js
@@ -6,6 +6,6 @@ const email = formInfo.get('email');
 
 const container = document.querySelector('.display-thanks')
 container.innerHTML = `
-    <h2>Thanks for join to our Newsletter ${completeName}}</h2>
+    <h2>Thanks for join to our Newsletter <span id="name">${completeName}</span></h2>
     <p>All our weekly emails are getting to <strong>${email}</strong></p>
 `;
