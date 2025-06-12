71f5396809a72bb093604bd0b40be2a19b078ad2
educalle23 <edu.lds.23@gmail.com>
2025-06-12 18:26:32 -0500

    js

--- finalproject/scripts/index.js
+++ finalproject/scripts/index.js
@@ -2,3 +2,14 @@ import { initHamburgerMenu } from './navigation.js';
 
 initHamburgerMenu();
 
+document.addEventListener('DOMContentLoaded', () => {
+    const bookBtns = document.querySelectorAll('.book-but');
+    
+    if (bookBtns.length > 0) {
+        bookBtns.forEach(btn => {
+            btn.addEventListener('click', () => {
+                window.location.href = 'services.html';
+            });
+        });
+    }
+});
